984578a6ffb39fbbc33000638735ca2a25b0017a
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2021-07-12 23:48:41 +0300

    Async bir şekilde olayları çalıştırma sorununu çözdüm!

--- index.js
+++ index.js
@@ -229,18 +229,13 @@ console.info("[BİLGİ] Basit Altyapı - by Kıraç Armağan Önal");
       ([eventName, events]) => {
         console.info(`[BİLGİ] Event "${eventName}" için ${events.length} dinleyici yüklendi!`);
         client.on(eventName, (...args) => {
-          events.forEach(event => {
-            if (!event.disabled) {
-              event.onEvent(...args);
-            }
-          })
-          // Nedenini bilmediğim bir şekilde sadece araya sync birşey
-          // sıkıştırınca çalışıyor. Örneğin console.log
-          // chillout.forEach(events, (event) => {
-          //   if (!event.disabled) {
-          //     event.onEvent(...args);
-          //   }
-          // });
+          setTimeout(() => {
+            chillout.forEach(events, (event) => {
+              if (!event.disabled) {
+                event.onEvent(...args);
+              }
+            });
+          },0)
         });
       }
     )
