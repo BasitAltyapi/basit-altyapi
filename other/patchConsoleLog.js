8b81a35fd80c8d7977140df641a48f52d82ff05e
Lyertia-Senpai <lyertia@lyessa.live>
2021-07-30 21:35:25 +0300

    Update patchConsoleLog.js

--- other/patchConsoleLog.js
+++ other/patchConsoleLog.js
@@ -1,7 +1,7 @@
 try {
   require("chalk");
 } catch {
-  console.warn("Bu işlemi yapamadan önce gerkli modülleri indirmeniz gerkiyor.")
+  console.warn("Bu işlemi yapmadan önce gerekli modülleri indirmeniz gerekiyor.")
   console.warn("-> yarn install");
   process.exit(-1);
 }
