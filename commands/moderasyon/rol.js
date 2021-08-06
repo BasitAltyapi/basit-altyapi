72560da07c9b311f006803d2b1cdc60ad339c13a
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2021-08-07 01:38:05 +0300

    daha fazla moderasyon komutu

---
+++ commands/moderasyon/rol.js
@@ -0,0 +1,59 @@
+module.exports = new (require("../../types/Command"))({
+  type: "SUB_COMMAND",
+  name: "moderasyon",
+  subName: "rol",
+  description: "Bir kullanıcıdan rol alamanızı sağlar.",
+  options: [
+    {
+      type: "USER",
+      name: "uye",
+      description: "Rol alacağanız kişi.",
+      required: true
+    },
+    {
+      type: "ROLE",
+      name: "rol",
+      description: "Alınacak rol.",
+      required: true
+    },
+    {
+      type: "STRING",
+      name: "durum",
+      description: "Rol verilsinmi alınsınmı?",
+      required: true,
+      choices: [
+        {
+          name: "Rol Ver",
+          value: "true"
+        },
+        {
+          name: "Rol Al",
+          value: "false"
+        }
+      ]
+    }
+  ],
+  async onCommand(interaction, other) {
+    /** @type {import("discord.js").GuildMember} */
+    let targetMember = interaction.options.getMember("uye", true);
+    let targetRole = interaction.options.getRole("rol", true);
+    let stateString = interaction.options.getString("durum", true);
+    let state = stateString == "true" ? true : false;
+
+    try {
+      if (state) {
+        if (!targetMember.roles.cache.has(targetRole)) await targetMember.roles.add([targetRole]);
+        interaction.reply(`**${targetMember.user.tag}** adlı üyeye **${targetRole.name}** rülü verildi.`);
+      } else {
+        if (targetMember.roles.cache.has(targetRole)) await targetMember.roles.remove([targetRole]);
+        interaction.reply(`**${targetMember.user.tag}** adlı üyeden **${targetRole.name}** rülü alındı.`);
+      }
+    } catch (err) {
+      interaction.reply(`**${targetMember.user.tag}** adlı üyenin rol durumu değiştirilken bir sorun ile karşılaşıldı. \`${err}\``);
+    }
+  },
+  perms: {
+    bot: ["MANAGE_ROLES"],
+    user: ["MANAGE_ROLES"]
+  }
+})
---
+++ commands/moderasyon/ses-sustur.js
@@ -0,0 +1,52 @@
+module.exports = new (require("../../types/Command"))({
+  type: "SUB_COMMAND",
+  name: "moderasyon",
+  subName: "ses-sustur",
+  description: "Bir kullanıcının ses susturmasını açıp kapamanızı sağlar.",
+  options: [
+    {
+      type: "USER",
+      name: "uye",
+      description: "Rol verceğeniz kişi.",
+      required: true
+    },
+    {
+      type: "STRING",
+      name: "durum",
+      description: "Susturulma durumu.",
+      required: true,
+      choices: [
+        {
+          name: "Sustur (Mute)",
+          value: "true"
+        },
+        {
+          name: "Susturma Aç (Unmute)",
+          value: "false"
+        }
+      ]
+    }
+  ],
+  async onCommand(interaction, other) {
+    /** @type {import("discord.js").GuildMember} */
+    let targetMember = interaction.options.getMember("uye", true);
+    let stateString = interaction.options.getString("durum", true);
+    let state = stateString == "true" ? true : false;
+    
+    try {
+      await targetMember.voice.setMute(state);
+      if (state) {
+        interaction.reply(`**${targetMember.user.tag}** adlı üye sesli kanallarda **susturuldu**.`);
+      } else {
+        interaction.reply(`**${targetMember.user.tag}** adlı üyenin ses kanallarında **susuturması açıldı**.`);
+      }
+    } catch (err) {
+      interaction.reply(`**${targetMember.user.tag}** adlı üyenin susuturma durumu değiştirelemedi. \`${err}\``);
+    }
+
+  },
+  perms: {
+    bot: ["MANAGE_CHANNELS"],
+    user: ["MANAGE_CHANNELS"]
+  }
+})
