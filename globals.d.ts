9dd6a9fb74464b0a6099533895ced829a3c4292f
wioniqle-q <69215407+wioniqle-q@users.noreply.github.com>
2021-11-30 01:12:29 +0300

    Update globals.d.ts

--- globals.d.ts
+++ globals.d.ts
@@ -1,23 +1,22 @@
 // Global değişkenlerde otomatik tamamlama desteği için.
 
 interface Underline {
-  interactions: import("discord.js").Collection<string, import("./types/Interaction") >
-  events: import("discord.js").Collection<string, import("./types/Event") >
-  config: import("./types/Config");
-  client: import("discord.js").Client;
-  Interaction: typeof import("./types/Interaction"),
-  SlashCommand: typeof import("./types/SlashCommand"),
-  MessageAction: typeof import("./types/MessageAction"),
-  UserAction: typeof import("./types/UserAction"),
-  Event: typeof import("./types/Event"),
-  Button: typeof import("./types/Button"),
-  SelectMenu: typeof import("./types/SelectMenu"),
+    interactions: import("discord.js").Collection<string, import("./types/Interaction")>
+    events: import("discord.js").Collection<string, import("./types/Event")>
+    config: import("./types/Config");
+    client: import("discord.js").Client;
+    Interaction: typeof import("./types/Interaction"),
+    SlashCommand: typeof import("./types/SlashCommand"),
+    MessageAction: typeof import("./types/MessageAction"),
+    UserAction: typeof import("./types/UserAction"),
+    Event: typeof import("./types/Event"),
+    Button: typeof import("./types/Button"),
+    SelectMenu: typeof import("./types/SelectMenu"),
 
-  reload(): Promise<any>;
+    reload(): Promise<any>;
 
-  [key: string | number]: any
+    [key: string | number]: any
 }
-
 
 
 declare var Underline: Underline;
