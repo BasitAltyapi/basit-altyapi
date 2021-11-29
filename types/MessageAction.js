9320086db737663350e8ba674ac0f25398a5f11f
wioniqle-q <69215407+wioniqle-q@users.noreply.github.com>
2021-11-30 01:15:49 +0300

    Update MessageAction.js

--- types/MessageAction.js
+++ types/MessageAction.js
@@ -1,22 +1,39 @@
-const { ContextMenuInteraction } = require("discord.js");
+const {ContextMenuInteraction} = require("discord.js");
 const Interaction = require("./Interaction");
 
 class MessageAction extends Interaction {
 
-  /** @param {Interaction.TOmittedInteraction & {name: string, onInteraction(interaction: ContextMenuInteraction, other: Interaction.IOther)}} arg */
-  constructor (arg = { }) {
-    super({
-      type: "COMMAND",
-      actionType: "MESSAGE",
-      ...arg
-    })
-  }
-  isMessageActionCommand() { return true; }
-  isSelectMenu() { return false; }
-  isButton() { return false; }
-  isChatActionCommand() { return false; }
-  isUserActionCommand() { return false; }
-  toJSON() {}
+    /** @param {Interaction.TOmittedInteraction & {name: string, onInteraction(interaction: ContextMenuInteraction, other: Interaction.IOther)}} arg */
+    constructor(arg = {}) {
+        super({
+            type: "COMMAND",
+            actionType: "MESSAGE",
+            ...arg
+        })
+    }
+
+    isMessageActionCommand() {
+        return true;
+    }
+
+    isSelectMenu() {
+        return false;
+    }
+
+    isButton() {
+        return false;
+    }
+
+    isChatActionCommand() {
+        return false;
+    }
+
+    isUserActionCommand() {
+        return false;
+    }
+
+    toJSON() {
+    }
 }
 
 module.exports = MessageAction;
