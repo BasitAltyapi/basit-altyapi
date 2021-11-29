2b39532bbc72f64f7d40ac6df1c4b7e073163f84
wioniqle-q <69215407+wioniqle-q@users.noreply.github.com>
2021-11-30 01:18:30 +0300

    Update patchConsoleLog.js

--- other/patchConsoleLog.js
+++ other/patchConsoleLog.js
@@ -1,53 +1,64 @@
 try {
-  require("chalk");
+    require("chalk");
 } catch {
-  console.warn("Bu işlemi yapmadan önce gerekli modülleri indirmeniz gerekiyor.")
-  console.warn("-> yarn install");
-  process.exit(-1);
+    console.warn("Bu işlemi yapmadan önce gerekli modülleri indirmeniz gerekiyor.")
+    console.warn("-> yarn install");
+    process.exit(-1);
 }
 
 const chalk = require("chalk");
 
 {
-  let originalLog = console.log;
-
-  let log = (...args)=>{
+  let {log: originalLog} = console, log = (...args) => {
     originalLog(chalk.blackBright(`[${new Date().toLocaleTimeString()}]`), ...args)
-  }
+  };
 
-  console.info = function (...args) {
-    args = args.map(i => {
-      if (typeof i == "string") return chalk.blueBright(i)
-      return i
-    })
-    log(...args);
-  }
+  /**
+     * @param {*[][]} args
+     */
+    console.info = function (...args) {
+        args = args.map(i => {
+          return typeof i == "string" ? chalk.blueBright(i) : i;
+        })
+        log(...args);
+    }
 
-  console.warn = function (...args) {
-    args = args.map(i => {
-      if (typeof i == "string") return chalk.yellowBright(i)
-      return i
-    })
-    log(...args);
-  }
+    /**
+     * @param {*[][]} args
+     */
+    console.warn = function (...args) {
+        args = args.map(i => {
+            if (!(typeof i != "string")) return chalk.yellowBright(i)
+            return i
+        })
+        log(...args);
+    }
 
-  console.error = function (...args) {
-    args = args.map(i => {
-      if (typeof i == "string") return chalk.redBright(i)
-      return i
-    })
-    log(...args);
-  }
+    /**
+     * @param {*[][]} args
+     */
+    console.error = function (...args) {
+        args = args.map(i => {
+          return typeof i == "string" ? chalk.redBright(i) : i;
+        })
+        log(...args);
+    }
 
-  console.debug = function(...args) {
-    args = args.map(i => {
-      if (typeof i == "string") return chalk.magentaBright(i)
-      return i
-    })
-    log(...args);
-  }
+    /**
+     * @param {*[][]} args
+     */
+    console.debug = function (...args) {
+        args = args.map(i => {
+            if (typeof i == "string") return chalk.magentaBright(i)
+            return i
+        })
+        log(...args);
+    }
 
-  console.log = function(...args) {
-    log(...args);
-  }
+    /**
+     * @param {*[][]} args
+     */
+    console.log = function (...args) {
+        log(...args);
+    }
 }
