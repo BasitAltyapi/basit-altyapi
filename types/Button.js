f24e843266f60b97ac57862033999c1b53a1d42e
wioniqle-q <69215407+wioniqle-q@users.noreply.github.com>
2021-11-30 01:16:47 +0300

    Update Button.js

--- types/Button.js
+++ types/Button.js
@@ -1,29 +1,47 @@
-const { MessageButton } = require("discord.js");
+const {MessageButton} = require("discord.js");
 const Interaction = require("./Interaction");
 
 class Button extends Interaction {
-  /** @param {Interaction.TOmittedInteraction & Interaction.Button} arg */
-  constructor (arg = { }) {
-    super({
-      _type: "noDeployInteraction",
-      actionType: "BUTTON",
-      ...arg
-    })
-  }
-  isButton() { return true; }
-  toJSON() {
-    let button = new MessageButton()
-      .setCustomId(this.id)
-      .setStyle(this.options.style);
-    if (this.options.emoji) button.setEmoji(this.options.emoji);
-    if (this.options.label) button.setLabel(this.options.label);
-    if (this.options.url) button.setURL(this.options.url);
-    return button;
-  }
-  isSelectMenu() { return false; }
-  isChatActionCommand() { return false; }
-  isUserActionCommand() { return false; }
-  isMessageActionCommand() { return false; }
+  arg = {};
+    /** @param {Interaction.TOmittedInteraction & Interaction.Button} arg */
+    constructor(arg = {}) {
+        super({
+            _type: "noDeployInteraction",
+            actionType: "BUTTON",
+            ...arg
+        })
+      this.arg = arg;
+    }
+
+    isButton() {
+        return true;
+    }
+
+    toJSON() {
+        let button = new MessageButton()
+            .setCustomId(this.id)
+            .setStyle(this.options.style);
+        if (this.options.emoji) button.setEmoji(this.options.emoji);
+        if (this.options.label) button.setLabel(this.options.label);
+        if (this.options.url) button.setURL(this.options.url);
+        return button;
+    }
+
+    isSelectMenu() {
+        return false;
+    }
+
+    isChatActionCommand() {
+        return false;
+    }
+
+    isUserActionCommand() {
+        return false;
+    }
+
+    isMessageActionCommand() {
+        return false;
+    }
 }
 
 module.exports = Button;
