405cce2cf0635996e546bcdc4186473565950fb3
Erdem <74366943+ErdemGKSL@users.noreply.github.com>
2022-07-18 15:22:23 +0300

    ozur dilerim

--- watchChanges.js
+++ watchChanges.js
@@ -44,47 +44,37 @@ let TypeQueue = new FunctionQueue(15000, async () => {
 let wait = (s) => new Promise((resolve => setTimeout(() => resolve(true), s)));
 (async () => {
   await wait(1000);
-  let eventsPath = resolve("./events");
-  await makeSureFolderExists(eventsPath);
-  const eventsWatcher = chokidar.watch(eventsPath, { persistent: true });
-  eventsWatcher
-    .on('add', () => TypeQueue.trigger())
-    .on('change', () => TypeQueue.trigger())
-    .on('unlink', () => TypeQueue.trigger())
-    .on('error', error => console.log(`Events Watcher Error: ${error}`))
-    .on('ready', () => console.log('Eventler izleniyor'))
 
-  let InteractionsPath = resolve("./interactions");
-
-  await makeSureFolderExists(InteractionsPath);
-  const InteractionsWatcher = chokidar.watch(InteractionsPath, { persistent: true });
-  InteractionsWatcher
-    .on('add', () => TypeQueue.trigger())
-    .on('change', () => TypeQueue.trigger())
-    .on('unlink', () => TypeQueue.trigger())
-    .on('error', error => console.log(`Interactions Watcher Error: ${error}`))
-    .on('ready', () => console.log('Interaksiyonlar izleniyor'))
-
-  let pluginsPath = resolve("./plugins");
-
-  await makeSureFolderExists(pluginsPath);
-  const pluginsWatcher = chokidar.watch(pluginsPath, { persistent: true });
-  pluginsWatcher
-    .on('add', () => TypeQueue.trigger())
-    .on('change', () => TypeQueue.trigger())
-    .on('unlink', () => TypeQueue.trigger())
-    .on('error', error => console.log(`plugins Watcher Error: ${error}`))
-    .on('ready', () => console.log('Pluginler izleniyor'))
-
-  let localesPath = resolve("./locales");
-
-  await makeSureFolderExists(localesPath);
-  const localesWatcher = chokidar.watch(localesPath, { persistent: true });
-  localesWatcher
-    .on('add', () => TypeQueue.trigger())
-    .on('change', () => TypeQueue.trigger())
-    .on('unlink', () => TypeQueue.trigger())
-    .on('error', error => console.log(`Locales Watcher Error: ${error}`))
-    .on('ready', () => console.log('Dil dosyaları izleniyor'))
-
+  [
+    {
+      path: "./events",
+      name: "Events",
+      text: "Eventler izleniyor.",
+    },
+    {
+      path: "./interactions",
+      name: "Interactions",
+      text: 'Interaksiyonlar izleniyor',
+    },
+    {
+      path: "./plugins",
+      name: "Plugins",
+      text: 'Pluginler izleniyor',
+    },
+    {
+      path: "./locales",
+      name: "Locales",
+      text: 'Dil dosyaları izleniyor',
+    }
+  ].forEach(async (ctx) => {
+    let path = resolve(ctx.path);
+    await makeSureFolderExists(path);
+    const theWatcher = chokidar.watch(path, { persistent: true });
+    theWatcher
+      .on('add', () => TypeQueue.trigger())
+      .on('change', () => TypeQueue.trigger())
+      .on('unlink', () => TypeQueue.trigger())
+      .on('error', error => console.log(`${ctx.name} Watcher Error: ${error}`))
+      .on('ready', () => console.log(ctx.text))
+  });
 })();
